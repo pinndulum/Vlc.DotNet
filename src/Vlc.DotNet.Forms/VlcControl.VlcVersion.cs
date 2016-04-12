3dc6b6afda24c6b186b456025c1d75d647d253f3
jonathan <jonathan@redtouchmedia.com>
2016-04-12 08:52:18 -0600

    added vlc version support to the VlcControl

---
+++ src/Vlc.DotNet.Forms/VlcControl.VlcVersion.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Vlc.DotNet.Forms
+{
+    public partial class VlcControl
+    {
+        public string VlcVersion
+        {
+            get
+            {
+                var vlcVersion = "Unknown Version";
+                if (myVlcMediaPlayer != null && myVlcMediaPlayer.Manager != null)
+                    vlcVersion = myVlcMediaPlayer.Manager.VlcVersion;
+                return vlcVersion;
+            }
+        }
+    }
+}
